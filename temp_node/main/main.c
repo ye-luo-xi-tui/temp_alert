e195b6f831cb8b509feccc48a304d27be30af565
yezi <1536117624@qq.com>
2022-12-13 21:13:30 +0800

    Delete test codes.Add temperature sensor initial.

--- temp_node/main/main.c
+++ temp_node/main/main.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "driver/twai.h"
-#include "temp.h"
+#include "driver/temperature_sensor.h"
+
+void initTempSensor(temperature_sensor_handle_t* temp_sensor_handle,double min_temp,double max_temp)
+{
+
+  temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(min_temp,max_temp);
+  ESP_ERROR_CHECK(temperature_sensor_install(&temp_sensor_config,temp_sensor_handle));
+  ESP_ERROR_CHECK(temperature_sensor_enable(*temp_sensor_handle));
+}
 
 void app_main(void)
 {
-  gpioInit();
-
-  printf("temp: %d",Ds18b20ReadTemp());
-
+  temperature_sensor_handle_t temp_sensor = NULL;
+  initTempSensor(&temp_sensor,10,50);
 }
